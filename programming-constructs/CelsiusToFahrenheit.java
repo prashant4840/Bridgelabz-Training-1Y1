4525d1d944c7edddfd6b43367c99315aab174ba3
prashant4840 <prashantsharma4830@gmail.com>
2026-01-09 22:48:57 +0530

    problem03

--- programming-constructs/CelsiusToFahrenheit.java
+++ programming-constructs/CelsiusToFahrenheit.java
@@ -1,13 +1,10 @@
 public class CelsiusToFahrenheit {
     public static void main(String[] args) {
 
-       
         double celsius = 25.0;
 
-        
         double fahrenheit = (celsius * 9.0 / 5.0) + 32;
 
-        
         System.out.println("Temperature in Celsius: " + celsius);
         System.out.println("Temperature in Fahrenheit: " + fahrenheit);
     }
