c013b9f4ced9ae2dac8fe80028533bc44d185806
prashant4840 <prashantsharma4830@gmail.com>
2026-01-09 22:29:46 +0530

    problem02

---
+++ programming-constructs/SumOfTwoNumbers.java
@@ -0,0 +1,10 @@
+public class SumOfTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+
+        int sum = num1 + num2;
+
+        System.out.println("Sum = " + sum);
+    }
+}
