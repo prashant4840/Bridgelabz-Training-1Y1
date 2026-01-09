10c100bb1c12a80c1ccba75b1c01f29d9e940661
prashant4840 <prashantsharma4830@gmail.com>
2026-01-09 23:14:35 +0530

    problem02

--- programming-constructs/SumOfTwoNumbers.java
+++ programming-constructs/SumOfTwoNumbers.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
+
 public class SumOfTwoNumbers {
     public static void main(String[] args) {
-        int num1 = Integer.parseInt(args[0]);
-        int num2 = Integer.parseInt(args[1]);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
 
         int sum = num1 + num2;
 
         System.out.println("Sum = " + sum);
+
+        scanner.close();
     }
 }
-
