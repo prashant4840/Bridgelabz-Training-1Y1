df2012422c84319744839b2911af69e350947090
prashant4840 <prashantsharma4830@gmail.com>
2026-01-10 16:23:11 +0530

    programming constructs

--- programming-constructs/CelsiusToFahrenheit.java
+++ programming-constructs/CelsiusToFahrenheit.java
@@ -4,14 +4,14 @@ public class CelsiusToFahrenheit {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Input temperature in Celsius
+       
         System.out.print("Enter temperature in Celsius: ");
         double celsius = scanner.nextDouble();
 
-        // Convert to Fahrenheit
+    
         double fahrenheit = (celsius * 9.0 / 5.0) + 32;
 
-        // Output result
+        
         System.out.println("Temperature in Fahrenheit: " + fahrenheit);
 
         scanner.close();
