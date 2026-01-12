be722a1b264ded1d03bc3480bad439a0060d540e
prashant4840 <prashantsharma4830@gmail.com>
2026-01-12 16:34:58 +0530

    selfproblem05

--- basic-programs/KilometersToMiles.java
+++ basic-programs/KilometersToMiles.java
@@ -4,10 +4,8 @@ public class KilometersToMiles {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        
         System.out.print("Enter distance in kilometers: ");
         double kilometers = sc.nextDouble();
-
         
         double miles = kilometers * 0.621371;
 
