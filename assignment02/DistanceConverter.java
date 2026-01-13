36db53165cf732b49c4a14a034ce6c0984353741
prashant4840 <prashantsharma4830@gmail.com>
2026-01-13 22:21:42 +0530

    problem14

--- assignment02/DistanceConverter.java
+++ assignment02/DistanceConverter.java
@@ -5,12 +5,15 @@ public class DistanceConverter {
 
         Scanner scanner = new Scanner(System.in);
 
+
         System.out.print("Enter distance in feet: ");
         double distanceInFeet = scanner.nextDouble();
+
 
         double distanceInYards = distanceInFeet / 3;
         double distanceInMiles = distanceInYards / 1760;
 
+        
         System.out.println("Distance in feet is " + distanceInFeet);
         System.out.println("Distance in yards is " + distanceInYards);
         System.out.println("Distance in miles is " + distanceInMiles);
