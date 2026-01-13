a40c6ee21f7c0a425128367a29de3e10b24b9d7e
prashant4840 <prashantsharma4830@gmail.com>
2026-01-13 22:28:57 +0530

    sameproblem01

--- assignment03/BasicCalculator.java
+++ assignment03/BasicCalculator.java
@@ -14,6 +14,7 @@ public class BasicCalculator {
         double multiplication = number1 * number2;
         double division = number1 / number2;
 
+        
         System.out.println(
             "The addition, subtraction, multiplication and division value of 2 numbers\n" +
             number1 + " and " + number2 + " is " +
