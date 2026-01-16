22a534a2bc64e2630272ca258a01037cffc38441
prashant4840 <prashantsharma4830@gmail.com>
2026-01-16 17:37:51 +0530

    assignment01/selfproblem05

--- basic-programs/KmToMiles.java
+++ basic-programs/KmToMiles.java
@@ -4,7 +4,11 @@ public class KmToMiles {
         double kilometers = 10.8;
         double miles = kilometers * 1.6;
 
+
         System.out.println("The distance " + kilometers + " km in miles is " + miles);
+
+        
     }
+
     
 }
