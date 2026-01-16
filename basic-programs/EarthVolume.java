6a8a41e06e158251d498853d564931b7a398b369
prashant4840 <prashantsharma4830@gmail.com>
2026-01-16 17:42:34 +0530

    assignment02/problem07

--- basic-programs/EarthVolume.java
+++ basic-programs/EarthVolume.java
@@ -3,11 +3,15 @@ public class EarthVolume {
         double radiusKm = 6378;
         double pi = Math.PI;
 
+        
         double volumeKm3 = (4.0 / 3.0) * pi * Math.pow(radiusKm, 3);
+
 
         double radiusMiles = radiusKm * 0.621371;
 
+
         double volumeMiles3 = (4.0 / 3.0) * pi * Math.pow(radiusMiles, 3);
+
 
         System.out.println(
             "The volume of earth in cubic kilometers is " + volumeKm3 +
