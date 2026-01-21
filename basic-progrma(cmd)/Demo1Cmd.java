25e131cf8029fa813eb11e920a5ac8be0ac0f18a
prashant4840 <prashantsharma4830@gmail.com>
2026-01-21 19:10:18 +0530

    command-line-arguments

--- basic-progrma(cmd)/Demo1Cmd.java
+++ basic-progrma(cmd)/Demo1Cmd.java
@@ -1,15 +1,21 @@
 public class Demo1Cmd{
 static int x = 50;
 int z = 60;
+
+
 public static void m2(){
 int y = 30;
 System.out.println("m2 local var "+ y);
 }
+
+
 public static void m1(){
 int y = 20;
 System.out.println("m1 local var "+ y);
 m2();
 }
+
+
 public static void main(String[] args){
 int y = 10;
 System.out.println("HelloWorld");
