5d0f53bb52c8e0e8cee229681f0cdbc6d78328e3
prashant4840 <prashantsharma4830@gmail.com>
2026-01-22 14:58:34 +0530

    Workshop

---
+++ basic-program/Loops.java
@@ -0,0 +1,49 @@
+
+public class Loops{
+public static void main(String[] args){
+
+//for loops
+for ( int i = 0 ; i<10; i++){
+System.out.println("i = " +i);
+
+}
+
+// while loops
+
+int j = 10;
+while(j>0){
+System.out.println("j :" +j);
+j--;
+
+}
+
+
+int k = 10;
+
+// do while loops
+
+do {
+    System.out.println("j : " + j);
+    k--;
+} while (k > 0);
+
+ }
+}
+
+// general usecase increment and decrement function
+
+int i =0 ;
+System.out.println("vel " +i);
+i++;
+System.out.println("vel a" + i++);
+System.out.println("vel b" +i);
+i++;
+System.out.println("vel c" + i++);
+
+System.out.println("vel d" + i++);
+
+
+
+
+
+
