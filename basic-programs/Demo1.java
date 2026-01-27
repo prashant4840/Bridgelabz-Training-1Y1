4559722c728adf560fe6873aea8412b38bda4846
prashant4840 <prashantsharma4830@gmail.com>
2026-01-28 00:43:18 +0530

    [Prashant Sharma] basic-programs

--- basic-programs/Demo1.java
+++ basic-programs/Demo1.java
@@ -1,15 +1,19 @@
 public class Demo1{
 static int x = 50;
+
 int z = 60;
-public static void m2(){
-int y = 30;
+public static void m2()
+
+{int y = 30;
 System.out.println("m2 local var "+ y);
 }
+
 public static void m1(){
 int y = 20;
 System.out.println("m1 local var "+ y);
 m2();
 }
+
 public static void main(String[] args){
 int y = 10;
 System.out.println("HelloWorld");
