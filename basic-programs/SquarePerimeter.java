98598cfbfbcd50b9338724f37a25527e4f48143c
prashant4840 <prashantsharma4830@gmail.com>
2026-01-27 19:03:24 +0530

    SquarePerimeter

--- basic-programs/SquarePerimeter.java
+++ basic-programs/SquarePerimeter.java
@@ -15,7 +15,7 @@ public class SquarePerimeter {
         System.out.println("The length of the side is " + side);
         System.out.println("whose perimeter is " + perimeter);
 
-        scanner.close();
+        
     }
     
 }
