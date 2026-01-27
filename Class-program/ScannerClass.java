7fbee3623b6891ed55d7dc354155572ce7b35ada
prashant4840 <prashantsharma4830@gmail.com>
2026-01-27 19:30:43 +0530

    taking-input-in-java

---
+++ Class-program/ScannerClass.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ScannerClass{
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		
+		int x = in.nextInt();
+		System.out.println("x: "+ x);
+	}
+}
