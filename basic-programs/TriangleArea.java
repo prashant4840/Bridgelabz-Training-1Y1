89d7a74b495acb662bd2c463070b832b7f00a9f4
prashant4840 <prashantsharma4830@gmail.com>
2026-01-27 19:02:01 +0530

    TriangleArea

--- basic-programs/TriangleArea.java
+++ basic-programs/TriangleArea.java
@@ -17,8 +17,6 @@ public class TriangleArea {
             "The area of the triangle in square inches is " + areaInSqInches +
             " and the area of the triangle in square centimeters is " + areaInSqCm
         );
-
-
         
     }
     
