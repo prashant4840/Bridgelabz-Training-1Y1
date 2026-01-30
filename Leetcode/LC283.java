d8edc07b1bf6ad5e193584ab3fa58687b0798916
prashant4840 <prashantsharma4830@gmail.com>
2026-01-30 12:46:57 +0530

    LC283

--- Leetcode/LC283.java
+++ Leetcode/LC283.java
@@ -1,23 +1,29 @@
-public class LC283 {
-    public static void moveZeroes(int[] nums) {
-        int l = 0;
 
-         for (int r = 0; r < nums.length; r++) {
-            if (nums[r] != 0) {
-                nums[l] = nums[r];
-                l++;
+public class LC283 {
+
+    public static int[] moveZeroes(int[] arr) {
+        int current = 0;
+        int zeroIndex = arr.length - 1;
+        int i = 0;
+
+        while (i < zeroIndex) {
+            if (arr[i] == 0) {
+                current = i;
+
+                while (current < zeroIndex) {
+                    arr[current] = arr[current + 1];
+                    current++;
+                }
+
+                arr[zeroIndex] = 0;
+                zeroIndex--;
+            } else {
+                i++;
             }
         }
-        for (int r = l; r < nums.length; r++) {
-            nums[r] = 0;
-        }
-    }
-    public static void main(String[] args) {
-        int[] nums = {0, 1, 0, 3, 12};
-        moveZeroes(nums);
 
-        for (int n : nums) {
-            System.out.print(n + " ");
-        }
+        return arr;
     }
 }
+
+
