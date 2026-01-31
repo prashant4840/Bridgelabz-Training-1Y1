7704dc7aab4dd17065737fa329b58403f3630c76
prashant4840 <prashantsharma4830@gmail.com>
2026-01-31 18:58:14 +0530

    VotingEligibilityArray

---
+++ Leetcode/LC283 - optimised.java
@@ -0,0 +1,29 @@
+
+
+public class LC283 {
+
+    public static void moveZeroes(int[] nums) {
+        int l = 0;
+
+       
+         for (int r = 0; r < nums.length; r++) {
+            if (nums[r] != 0) {
+                nums[l] = nums[r];
+                l++;
+            }
+        }
+
+        for (int r = l; r < nums.length; r++) {
+            nums[r] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+        moveZeroes(nums);
+
+        for (int n : nums) {
+            System.out.print(n + " ");
+        }
+    }
+}
