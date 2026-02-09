ddb55acaf71ced483f16d12ffc1b3b26d1a0f101
prashant4840 <prashantsharma4830@gmail.com>
2026-02-09 20:38:01 +0530

    [Prashant Sharma] LC242

---
+++ Leetcode/LC242.java
@@ -0,0 +1,19 @@
+public class LC242 {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] arr = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
+        }
+        // Step 3: Verify counts
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
