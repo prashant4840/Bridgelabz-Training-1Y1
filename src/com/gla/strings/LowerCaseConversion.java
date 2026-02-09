a97f270f3a38f465734ab4ad71d2bc35884d28c8
prashant4840 <prashantsharma4830@gmail.com>
2026-02-09 22:50:59 +0530

    [Prashant Sharma] Level 1 LowerCaseConversion

--- src/com/gla/strings/LowerCaseConversion.java
+++ src/com/gla/strings/LowerCaseConversion.java
@@ -21,6 +21,7 @@ public class LowerCaseConversion {
             return false;
         }
 
+
         for (int i = 0; i < s1.length(); i++) {
             if (s1.charAt(i) != s2.charAt(i)) {
                 return false;
