d419e1744df39ebc190d27f092dcb062e701ff33
prashant4840 <prashantsharma4830@gmail.com>
2026-02-13 12:38:06 +0530

    OOPS Concepts

--- Class-program/package1/Sample3.java
+++ Class-program/package1/Sample3.java
@@ -1,23 +1,33 @@
 package com.gla.package1;
 
 public class Sample3 {
-    static void main() {
 //    System.out.println(Sample1.varB);
 //    Sample1 obj = new Sample1();
 //    System.out.println(obj.varA);
 //
 //    Sample1.methodB();
-
+//
 //        obj.methodC();
-//        System.out.println(varD);
+//        System.out.println(Sample1.varD);
+//        Sample1 obj = new Sample1();
 //        System.out.println(obj.varC);
 //
-//        methodD();
+//    methodD();
 //        obj.methodC();
-        System.out.println(varD);
-        System.out.println(obj.varC);
+//
+//        System.out.println(Sample1.varF);
+//        Sample1 obj = new Sample1();
+//        System.out.println(obj.varE);
+//
+//        Sample1.methodF();
+//        obj.methodE();
 
-        methodD();
-        obj.methodC();
-    }
+
+        System.out.println(Sample1.varH);
+        Sample1 obj = new Sample1();
+        System.out.println(obj.varG);
+
+    Sample1.methodH();
+        obj.methodG();
+
 }
--- Class-program/package2/Sample2.java
+++ Class-program/package2/Sample2.java
@@ -1,14 +1,15 @@
 package com.gla.package2;
+import com.gla.package1.Sample1;
 
 public class Sample2 {
-    static void main(){
+    static void main(String[] args) {
 
-            System.out.println(Sample1.varB);
-    Sample1 obj = new Sample1();
-    System.out.println(obj.varA);
-
-    Sample1.methodB();
-
-
+//        obj.methodE();
+//        System.out.println(Sample1.varF);
+//        Sample1 obj = new Sample1();
+//        System.out.println(Sample1.obj.varE);
+//
+//        methodF();
+//        obj.methodE();
     }
 }
